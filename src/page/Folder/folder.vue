386ab69ec51421713b9b6adf971c9b5c3803a792
linyouwei <yjlyw020150@163.com>
2018-08-15 22:04:07 +0800

    json格式化

--- src/page/Folder/folder.vue
+++ src/page/Folder/folder.vue
@@ -8,24 +8,57 @@
                 <el-col :span="20">
                     <el-container>
                         <el-header>
-                            <div id="catalog" class="btn-group">
-                                <el-button data-value="" >根目录</el-button>
+                            <div id="catalog" class="btn-group"  v-for="(item, index) in btnList">
+                                <el-button  @click="btnShow(index)" >{{item===''||item==='/'?'根目录':item}}</el-button>
                             </div>
                             <div class="btn-head custom-file-input">
-                                <el-form  class="demo-form-inline">
-                                    <el-button class="btn-head">选择要上传的文件</el-button>
-                                    <el-input class="btn-head file" type="file" placeholder="请输入内容">
-                                    </el-input>
-                                </el-form>
+                                <el-upload
+                                        class="upload-demo"
+                                        :action="uploadUrl()"
+                                        :on-change = 'handleUploadChange'
+                                        :on-success="handleUploadSuccess"
+                                        ref="upload">
+                                    <el-button size="small" type="primary" class="upload-btn">点击上传</el-button>
+                                </el-upload>
+
                             </div>
                             <div class="btn-group">
-                                <el-button >新建文件夹</el-button>
+                                <el-button  @click="dialogVisible = true">新建文件夹</el-button>
+                            </div>
+                            <el-dialog
+                                    title="新建文件夹"
+                                    :visible.sync="dialogVisible"
+                                    width="50%"
+                                 >
+                                     <el-input v-model="inpuVal" placeholder="输入文件夹" id="file-name"></el-input>
+                                    <span slot="footer" class="dialog-footer">
+                                    <el-button @click="dialogVisible = false">取 消</el-button>
+                                    <el-button type="primary" @click="addFloder">确 定</el-button>
+                                  </span>
+                            </el-dialog>
+                            <div class="btn-group btn-div" v-show="isShowDiv">
+                                <el-button v-bind:fileUrl="fileUrl" id="fileDownload" class="btn-float" @click="downloadFile"><i class="el-icon-download"></i></el-button>
+                                <el-button id="" class="btn-float"><i class="el-icon-success"></i></el-button>
+                                <el-button id="" class="btn-float"><i class="el-icon-view"></i></el-button>
+                                <el-button id="" class="btn-float"><i class="el-icon-remove"></i></el-button>
                             </div>
                         </el-header>
                         <el-main >
-                            <div>
-
-                            </div>
+                            <el-row>
+                                <el-col  :xs="12" :sm="4" :md="4" :lg="2" class="a-dir" v-for="dir in dirList" @click.native="toSubDir(dir)">
+                                    <a href="#"  >
+                                        <img src="../../images/folder.png" alt=""/>
+                                        <div class="gallery-title">{{dir}}</div>
+                                    </a>
+                               </el-col>
+                                 <el-col :xs="12":sm="4" :md="4" :lg="2" class="a-file" v-for="file in fileList" @click.native="bindFile(file)">
+                                    <a href="#"  >
+                                        <img v-if="checkFile(file)" src="../../images/txt.png" alt=""/>
+                                        <img v-else src="../../images/unknown.png" alt=""/>
+                                        <div class="gallery-title">{{file}}</div>
+                                    </a>
+                                </el-col>
+                            </el-row>
                         </el-main>
                     </el-container>
                     <el-main></el-main>
@@ -40,7 +73,15 @@
     export default {
             data () {
                 return{
-                    fileList:{}
+                    dialogVisible:false,
+                    btnList:[''],
+                    dirList:{},
+                    fileList:{},
+                    isTxt:true,
+                    split_path:'',
+                    inpuVal:'',
+                    isShowDiv:false,
+                    fileUrl : '',
                 }
              },
             components:{
@@ -50,11 +91,83 @@
                 this.showFile()
               },
              methods: {
-                 showFile(){
-                     let url = '/index/Index/read?path=/'
+                 showFile(path){
+                     if(!path) path='';
+                     let url = '/index/Index/read?path='+path
                      this.$api.get(url,null,r=>{
-                         this.fileList = r
-                     })
+                     },r=>{
+                     this.dirList = r.dir
+                     this.fileList = r.file
+                 })
+                 },
+                 checkFile(filename){
+                     let flag = false
+                     let arr = ['txt','TXT']
+                     var extend_name = filename.substring(filename.lastIndexOf(".")+1)
+                     for(let i=0;i<arr.length;i++){
+                         if(arr[0]===extend_name){
+                             flag =  true;
+                             break
+                         }
+                     }
+                     return flag
+                 },
+                 toSubDir(dir){//进去子目录
+                     //更新btnList
+                     this.btnList.push(dir)
+                     //得到路径
+                     let path_url = this.getPathUrl()
+                     this.showFile(path_url)
+
+                     //更新showFile
+                 },
+                 getPathUrl(){//得到路径
+                     let path_url = ''
+                     for(let i=0;i<this.btnList.length;i++){
+                         path_url += this.btnList[i]+'/'
+                     }
+                     return path_url
+                 },
+                 btnShow(index){//显示btn的
+                     // 0 1 2 3
+                     console.log(this.btnList);
+                     var len =this.btnList.length
+                     this.btnList.splice(index+1,len-1)
+                     let path_url = this.getPathUrl()
+                     this.showFile(path_url)
+                 },
+                 addFloder(){
+                       let filename = this.inpuVal;
+                       console.log(filename);
+                       let path = this.getPathUrl()
+                       let url ='/index/Index/create'
+                       let params = {}
+                        params.name = filename
+                        params.dir = this.getPathUrl()
+                        this.$api.post(url,params,r=>{
+                        },r=>{
+                            this.showFile(path)
+                            this.dialogVisible = false
+                        })
+                },
+                 uploadUrl(){
+                     return '/index/Index/upload?path='+this.getPathUrl()
+                 },
+                 handleUploadChange(){
+                     this.$refs.upload.submit()
+                 },
+                 handleUploadSuccess(){
+                     let path = this.getPathUrl()
+                     this.showFile(path)
+                 },
+                 bindFile(file){
+                     this.isShowDiv = true
+                     let path = this.getPathUrl()
+                     this.fileUrl = path+file
+                 },
+                 downloadFile(){
+                     console.log(this.fileUrl)
+                     window.open('/index/Index/download?file=' + this.fileUrl)
                  }
              }
     }
@@ -87,6 +200,33 @@
         opacity: 0;  /*css属性——opcity不透明度，取值0-1*/
         filter: alpha(opacity=0); /*兼容IE8及以下--filter属性是IE特有的，它还有很多其它滤镜效果，而filter: alpha(opacity=0); 兼容IE8及以下的IE浏览器(如果你的电脑IE是8以下的版本，使用某些效果是可能会有一个允许ActiveX的提示,注意点一下就ok啦)*/
         cursor: pointer;
+    }
+    .btn-float{
+        float: left;
+    }
+    .btn-div{
+        width: 300px;
+    }
+    .upload-btn{
+        display: inline-block;
+        line-height: 1;
+        white-space: nowrap;
+        cursor: pointer;
+        background: #fff;
+        border: 1px solid #dcdfe6;
+        color: #606266;
+        -webkit-appearance: none;
+        text-align: center;
+        -webkit-box-sizing: border-box;
+        box-sizing: border-box;
+        outline: 0;
+        margin: 0;
+        -webkit-transition: .1s;
+        transition: .1s;
+        font-weight: 500;
+        padding: 12px 20px;
+        font-size: 14px;
+        border-radius: 4px;
     }
 
 </style>
--- src/page/Json/Json.vue
+++ src/page/Json/Json.vue
@@ -7,9 +7,19 @@
                 </el-col>
                 <el-col :span="20">
                     <el-container>
-                        <el-header>
-                            123
-                        </el-header>
+                        <el-main>
+                           <el-input
+                                   type="textarea"
+                                   :rows="5"
+                                   placeholder="输入json数据"
+                                   v-model="textareas"
+                                   @blur="toFormat()"
+                                   >
+                           </el-input>
+                            <el-col :span="5">
+                                <pre id="editor" v-html="jsonResult" ></pre>
+                            </el-col>
+                        </el-main>
                     </el-container>
                     <el-main></el-main>
                 </el-col>
@@ -23,12 +33,57 @@
     export default {
             data () {
                 return{
-
+                    textareas:'',
+                    jsonResult:""
                 }
              },
             components:{
                 leftAside
             },
+            methods:{
+                toFormat(){
+                    let text = this.textareas
+                    console.log(text)
+                    //格式化
+                    var temp = '';
+                    var count = 0;//计算\t的个数
+                    var brace_cout = 0;//计算{的个数
+                    function output(data) {
+                        var start = data.indexOf("{");
+                        //"b":123
+                        if (start == -1) {
+                            temp = data.substring(data.indexOf(":") + 1)
+                            return temp;
+                        } else {
+                            var end = data.lastIndexOf("}")
+                            var tempData = data.substring(start + 1, end)
+                            //从tempData中截取:前面的数值。
+                            var contentIndex = tempData.indexOf(":")
+                            var content = tempData.substring(1, contentIndex + 1)
+                            count++
+                            var space = ''
+                            for (var j = 0; j < count; j++) {
+                                space += "\t"
+                            }
+                            var brace_space = ''
+                            if (brace_cout) {
+                                for (var i = 0; i < brace_cout; i++) {
+                                    brace_space += '\t'
+                                }
+                                brace_space += '}'
+                            } else {
+                                brace_space = '}'
+                            }
+                            brace_cout++;
+                            //拼接{ }s
+                            temp = "{<br/>" + space + content + output(tempData) + "<br/>" + brace_space
+                            return temp
+                        }
+                    }
+                    this.jsonResult = output(text)
+                }
+            }
+
     }
 </script>
 
